4d8eff970d1f9fdc7f778e9655ceb615a28285fc
davidlatwe <davidlatwe@gmail.com>
2022-12-11 19:47:03 +0800

    Add linux build script

---
+++ build_for_linux.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+BUILD_TYPE=Release
+
+BUILD_DIR=build/linux/$BUILD_TYPE
+
+mkdir -p $BUILD_DIR
+pushd $BUILD_DIR
+cmake ../../../physx -G Ninja \
+    -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+    -DDISABLE_CUDA_PHYSX=No \
+	-DPX_BUILDSNIPPETS=ON \
+	-DPX_BUILDSNIPPETS_RENDER=OFF
+
+cmake --build . --config $BUILD_TYPE
+popd
+
